e5645f9e0e13bce371d119312fe74453177aa050
Sam Bhattacharyya <sam@dotlearn.org>
2020-05-18 19:03:17 -0700

    worked out path parsing with relative bezier curves

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ const vvgl = (function(canvas, options={}) {
 
     const msgpack = require("msgpack-lite");
 
+    const math = require('mathjs');
+
     const vvgl = this;
 
     const {context, isWebGL2} = getContext(canvas);
@@ -300,6 +302,10 @@ const vvgl = (function(canvas, options={}) {
             let i = 0;
             let x,y;
 
+            let last_curve;
+
+            console.log(`New contour`);
+
             while (i < path.length) {
 
                 let element = path[i];
@@ -318,6 +324,9 @@ const vvgl = (function(canvas, options={}) {
                     x = [0, 0, path[i + 1], path[i + 1]].map(x => x + start[0]);
                     y = [0, 0, path[i + 2], path[i + 2]].map(y => y + start[1]);
 
+                    last_curve = path.slice(i, i+3);
+
+
                     start[0] = x[3];
                     start[1] = y[3];
 
@@ -327,40 +336,107 @@ const vvgl = (function(canvas, options={}) {
                     i += 3;
                 } else {
 
-                    let dx = path[i + 3];
-                    let dy = path[i + 4];
 
-                    // Put this computation into the player
+                    if(last_curve && !isNaN(last_curve[0])){
 
-                    let norm = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+                        let l = last_curve.length;
 
-                    let nx = -dy/norm;
-                    let ny = dx/norm;
-                    let fx = dx/norm;
-                    let fy = dy/norm;
+                        console.log(l);
 
-                    let f1 = path[i];
-                    let f2 =  path[i+1];
-                    let n =path[i+2];
+                        let dx = path[i + 2];
+                        let dy = path[i + 3];
 
-                    let c1x = n*nx + f1*fx;
-                    let c1y = n*ny + f1*fy;
+                        let norm = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
 
-                    let c2x = n*nx + f2*fx + dx;
-                    let c2y = n*ny + f2*fy + dy;
+                        let nx = -dy/norm;
+                        let ny = dx/norm;
+                        let fx = dx/norm;
+                        let fy = dy/norm;
 
 
-                    x = [0, c1x, c2x, dx].map(x => x + start[0]);
-                    y = [0,c1y, c2y,dy].map(y => y + start[1]);
+                        let f2 =  path[i];
+                        let n =path[i+1];
 
-                    start[0] = x[3];
-                    start[1] = y[3];
 
-                    this.length += 1;
-                    this.curves.push(...x, ...y);
+                        let c2x = n*nx + f2*fx + dx;
+                        let c2y = n*ny + f2*fy + dy;
 
-                    i += 5;
 
+                        let last_dx = last_curve[l-2];
+                        let last_dy = last_curve[l-1];
+
+                        let last_norm = Math.sqrt(Math.pow(last_dx, 2) + Math.pow(last_dy, 2));
+                        let last_nx = -last_dy/last_norm;
+                        let last_ny = last_dx/last_norm;
+                        let last_fx = last_dx/last_norm;
+                        let last_fy = last_dy/last_norm;
+
+                        let last_f2 = last_curve[l-4];
+                        let last_n = last_curve[l-3];
+
+                        let px = last_n*last_nx + last_f2*last_fx;
+                        let py = last_n*last_ny + last_f2*last_fy;
+
+                        let p = [px, py];
+                        let phat = math.divide(p, math.norm(p));
+
+                        let q = math.divide(n, math.dot(phat, [-nx, -ny]));
+
+                        let c1 = math.multiply(phat, q);
+
+                        let c1x = c1[0];
+                        let c1y = c1[1];
+
+
+                        x = [0, c1x, c2x, dx].map(x => x + start[0]);
+                        y = [0,c1y, c2y,dy].map(y => y + start[1]);
+
+                        start[0] = x[3];
+                        start[1] = y[3];
+
+                        this.length += 1;
+                        this.curves.push(...x, ...y);
+
+                        last_curve = path.slice(i, i+4);
+                        i += 4;
+
+                    } else{
+
+
+                        let dx = path[i + 3];
+                        let dy = path[i + 4];
+
+                        let norm = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+
+                        let nx = -dy/norm;
+                        let ny = dx/norm;
+                        let fx = dx/norm;
+                        let fy = dy/norm;
+
+                        let f1 = path[i];
+                        let f2 =  path[i+1];
+                        let n =path[i+2];
+
+                        let c1x = n*nx + f1*fx;
+                        let c1y = n*ny + f1*fy;
+
+                        let c2x = n*nx + f2*fx + dx;
+                        let c2y = n*ny + f2*fy + dy;
+
+
+                        x = [0, c1x, c2x, dx].map(x => x + start[0]);
+                        y = [0,c1y, c2y,dy].map(y => y + start[1]);
+
+                        start[0] = x[3];
+                        start[1] = y[3];
+
+                        this.length += 1;
+                        this.curves.push(...x, ...y);
+
+                        last_curve = path.slice(i, i+5);
+                        i += 5;
+
+                    }
 
                 }
 
@@ -869,8 +945,8 @@ const vvgl = (function(canvas, options={}) {
             gl.vertexAttribPointer(bezierLocations["color"], 4, gl.FLOAT, false, 52, 40 + 52*offset);
 
 
-            if(isWebGL2) gl.drawArraysInstanced(gl.TRIANGLE_FAN,  0, num_bezier_vertices, l);
-            else extensions.angle.drawArraysInstancedANGLE(gl.TRIANGLE_FAN,  0, num_bezier_vertices, l);
+         //   if(isWebGL2) gl.drawArraysInstanced(gl.TRIANGLE_FAN,  0, num_bezier_vertices, l);
+           // else extensions.angle.drawArraysInstancedANGLE(gl.TRIANGLE_FAN,  0, num_bezier_vertices, l);
 
         }
 
@@ -978,7 +1054,7 @@ const vvgl = (function(canvas, options={}) {
 
         render();
 
-        if(frame < update_manager.duration) return window.requestAnimationFrame(step);
+        if(frame <1 ) return window.requestAnimationFrame(step);
         else{
             console.log(`Done`);
         }
